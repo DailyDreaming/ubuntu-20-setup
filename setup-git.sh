54434fb4d635c59c861a19ab0e86be90675adb14
DailyDreaming <lblauvel@ucsc.edu>
2020-12-31 23:51:24 -0800

    git config

---
+++ setup-git.sh
@@ -0,0 +1,7 @@
+git config --global user.email "lblauvel@ucsc.edu"
+git config --global user.name "DailyDreaming"
+git config --global credential.helper store
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global core.excludesfile ~/.gitignore
+touch ~/.gitignore
+
